c3a3faefd976c555fe45b79ce0d18b5bdb881584
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2010-07-27 15:28:17 +0000

    - Rewrote de build BAT and added google's and yahoo's compiler

--- build/build-js.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-for %%x in (jet ajax anim base container cookie datasource datatable dragdrop flash forms imageloader json plasma resize sizzle tabs xsl) do (
-java -jar compiler.jar --js ../src/%%x.js --js_output_file %%x.min.js
-)
