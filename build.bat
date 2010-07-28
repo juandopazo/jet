f216296280c8b6113c185c3dfc8344e241febc1d
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2010-07-28 17:06:27 +0000

    - Fixed copy in build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,6 @@
 for %%x in (src/*.js) do (
     for /f "tokens=1,2 delims=." %%a in ("%%x") do (
         java -jar compiler.jar --js src/%%a.js --js_output_file build/%%a.min.js
-	copy src/%%a.js build/%%a.js
     )
 )
 for %%x in (src/*.css) do (
@@ -11,3 +10,5 @@ for %%x in (src/*.css) do (
         java -jar yuicompressor.jar src/%%a.css -o build/%%a.min.css
     )
 )
+copy src\*.js build
+copy src\*.css build
