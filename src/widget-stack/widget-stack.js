b11ee06dd5b8a78f7d09dadfecf29b3b1fdb92fe
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-02-26 21:04:49 -0300

    Added automatic resizing for the shim element

--- src/widget-stack/widget-stack.js
+++ src/widget-stack/widget-stack.js
@@ -5,7 +5,9 @@ jet().add('widget-stack', function ($) {
 		layers,
 		
 		ZINDEX = 'zIndex',
-		BOUNDING_BOX;
+		RESIZE = 'resize',
+		SHIM = 'shim',
+		BOUNDING_BOX = 'boundingBox';
 		
 	if (!jet.layers) {
 		jet.layers = [];
@@ -25,8 +27,7 @@ jet().add('widget-stack', function ($) {
 		this._shim = $('<iframe/>').attr({
 			src: 'javascript:false',
 			frameborder: 0
-		}).css({
-			position: 'absolute',
+		}).addClass(this.getClassName(SHIM)).css({
 			zIndex: zIndex
 		});
 	}
@@ -55,8 +56,10 @@ jet().add('widget-stack', function ($) {
 		EVENTS: {
 			
 			afterRender: function () {
-				if (this.get('shim')) {
+				if (this.get(SHIM)) {
 					this._shim.insertBefore(this.get(BOUNDING_BOX));
+					this.syncShim();
+					$.on(RESIZE, this.syncShim, this);
 				}
 			},
 			
@@ -64,13 +67,27 @@ jet().add('widget-stack', function ($) {
 				var shim = this._shim;
 				if (newVal) {
 					shim.insertBefore(this.get(BOUNDING_BOX));
+					$.on(RESIZE, this.syncShim, this);
 				} else {
 					shim.remove();
+					$.unbind(RESIZE, this.syncShim);
 				}
 			},
 			
 			afterZIndexChange: function (e, newVal) {
 				this.get(BOUNDING_BOX).css(ZINDEX, newVal);
+			},
+			
+			focus: function () {
+				this.bringToFront();
+			},
+			
+			blur: function () {
+				this.sendToBack();
+			},
+			
+			destroy: function () {
+				$.unbind(RESIZE, this.syncShim);
 			}
 			
 		},
@@ -88,6 +105,12 @@ jet().add('widget-stack', function ($) {
 			for (var i = 0, length = layers.length; i < length; i++) {
 				layers[i].set(ZINDEX, i);
 			}
+			return this;
+		},
+		
+		syncShim: function () {
+			var position = this.get(BOUNDING_BOX).offset();
+			this._shim.setPosition(position.left, position.top).width(position.width).height(position.height);
 			return this;
 		},
 		
