b830ff712de4c4d2013925ef43446c540803f2c3
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-03-09 15:03:15 -0300

    Fixed window and Jet references in JSON

--- src/json/js/json.js
+++ src/json/js/json.js
@@ -175,8 +175,8 @@
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
 
-if (!this.JSON) {
-    this.JSON = {};
+if (!window.JSON) {
+    $.JSON = window.JSON = {};
 }
 
 (function () {
