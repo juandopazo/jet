dfc34cc922d5b35731383ae2e08a690840449efa
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-04-26 12:44:38 -0300

    Class.mixin() added to OOP module Also all clases now have a static method create() that creates a new class extending it

--- build/jet.js
+++ build/jet.js
@@ -1297,7 +1297,8 @@ $.add({
 */
 jet.add('oop', function ($) {
 
-			var OP = Object.prototype;
+			var OP = Object.prototype,
+	$_Array = $.Array;
 
 /**
  * Utilities for object oriented programming in JavaScript.
@@ -1357,6 +1358,79 @@ $.extend = function (r, s, px, ax) {
 	
 	return r;
 };
+var Class = $.Class = function Class() {
+	var args = arguments;
+	
+	var classes = this._classes = [];
+	var constructor = this.constructor;
+	while (constructor !== Class) {
+		classes.unshift(constructor);
+		constructor = constructor.superclass.constructor;
+	}
+	
+	$_Array.each(classes, function (constructor) {
+		$_Array.each(constructor.EXTS || [], function (extension) {
+			extension.apply(this, args);
+		}, this);
+		if (constructor.prototype.hasOwnProperty('initializer')) {
+			constructor.prototype.initializer.apply(this, args);
+		}
+	}, this);
+}
+
+$.mix(Class, {
+	
+	NAME: 'Class',
+	
+	toString: function () {
+		return 'class ' + Class.NAME;
+	},
+	
+	create: function (name, superclass, extensions, attrs, proto) {
+		extensions = extensions || [];
+		
+		function BuiltClass() {
+			BuiltClass.superclass.constructor.apply(this, args);
+		}
+		$.extend(BuiltClass, superclass || Class, proto, attrs);
+		
+		$.mix(BuiltClass, {
+			NAME: name,
+			create: function (_name, _extensions, _attrs, _proto) {
+				return Class.create(_name, BuiltClass, _extensions, _attrs, _proto);
+			},
+			mixin: function () {
+				return Class.mixin(BuiltClass, Array.prototype.slice.call(arguments));
+			},
+			toString: function () {
+				return 'class ' + BuiltClass.NAME;
+			}
+		}, true);
+		
+		return Class.mixin(BuiltClass, extensions);
+	},
+	
+	mixin: function (constructor, extensions) {
+		if (!constructor.EXTS) {
+			constructor.EXTS = [];
+		}
+		constructor.EXTS.push.apply(constructor.EXTS, extensions);
+		
+		$_Array.each(extensions, function (extension) {
+			//$.mix(BuiltClass.prototype, extension.prototype);
+			$.Hash.each(extension, function (prop, val) {
+				if (!constructor[prop]) {
+					constructor[prop] = val;
+				} else if ($.Lang.isObject(constructor[prop]) && $.Lang.isObject(val)) {
+					$.mix(constructor[prop], val);
+				}
+			});
+		});
+		
+		return constructor;
+	}
+	
+}, true);
 			
 });/**
  * Node collections and DOM abstraction
--- src/oop/js/Class.js
+++ src/oop/js/Class.js
@@ -1,5 +1,5 @@
 
-$.Class = function Class() {
+var Class = $.Class = function Class() {
 	var args = arguments;
 	
 	var classes = this._classes = [];
@@ -10,50 +10,65 @@ $.Class = function Class() {
 	}
 	
 	$_Array.each(classes, function (constructor) {
+		$_Array.each(constructor.EXTS || [], function (extension) {
+			extension.apply(this, args);
+		}, this);
 		if (constructor.prototype.hasOwnProperty('initializer')) {
 			constructor.prototype.initializer.apply(this, args);
 		}
 	}, this);
 }
 
-function mixinExtensions(constructor, extensions) {
-	$_Array.each(extensions, function (extension) {
-		//$.mix(BuiltClass.prototype, extension.prototype);
-		$.Hash.each(extension, function (prop, val) {
-			if (!constructor[prop]) {
-				constructor[prop] = val;
-			} else if (Lang.isObject(constructor[prop]) && Lang.isObject(val)) {
-				$.mix(constructor[prop], val);
-			}
-		});
-	});
-}
-
-$.Class.create = function (name, superclass, extensions, attrs, proto) {
-	extensions = extensions || [];
+$.mix(Class, {
 	
-	function BuiltClass() {
-		var args = arguments;
-		BuiltClass.superclass.constructor.apply(this, args);
-		$_Array.each(BuiltClass.EXTS || [], function (extension) {
-			extension.apply(this, args);
-		}, this);
-	}
-	$.extend(BuiltClass, superclass || Class, proto, attrs);
+	NAME: 'Class',
 	
-	$.mix(BuiltClass, {
-		NAME: name,
-		EXTS: extensions,
-		extend: function (_name, _extensions, _attrs, _proto) {
-			return $.Class.create(_name, BuiltClass, _extensions, _attrs, _proto);
-		},
-		mixin: function () {
-			mixinExtensions(BuiltClass, Array.prototype.slice.call(arguments));
-			return BuiltClass;
+	toString: function () {
+		return 'class ' + Class.NAME;
+	},
+	
+	create: function (name, superclass, extensions, attrs, proto) {
+		extensions = extensions || [];
+		
+		function BuiltClass() {
+			BuiltClass.superclass.constructor.apply(this, args);
 		}
-	}, true);
+		$.extend(BuiltClass, superclass || Class, proto, attrs);
+		
+		$.mix(BuiltClass, {
+			NAME: name,
+			create: function (_name, _extensions, _attrs, _proto) {
+				return Class.create(_name, BuiltClass, _extensions, _attrs, _proto);
+			},
+			mixin: function () {
+				return Class.mixin(BuiltClass, Array.prototype.slice.call(arguments));
+			},
+			toString: function () {
+				return 'class ' + BuiltClass.NAME;
+			}
+		}, true);
+		
+		return Class.mixin(BuiltClass, extensions);
+	},
 	
-	mixinExtensions(BuiltClass, extensions);
+	mixin: function (constructor, extensions) {
+		if (!constructor.EXTS) {
+			constructor.EXTS = [];
+		}
+		constructor.EXTS.push.apply(constructor.EXTS, extensions);
+		
+		$_Array.each(extensions, function (extension) {
+			//$.mix(BuiltClass.prototype, extension.prototype);
+			$.Hash.each(extension, function (prop, val) {
+				if (!constructor[prop]) {
+					constructor[prop] = val;
+				} else if ($.Lang.isObject(constructor[prop]) && $.Lang.isObject(val)) {
+					$.mix(constructor[prop], val);
+				}
+			});
+		});
+		
+		return constructor;
+	}
 	
-	return BuiltClass;
-};
+}, true);
