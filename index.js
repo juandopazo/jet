fef3c53f4864491f74681c1184633c2c5ba93de8
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-05-04 12:23:43 -0300

    Fixing port in node instance

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (jet) is running..');
-}).listen(9788);
+}).listen(9806);
