a9969ffb23e19d601dea23cb1a2e6db7d53e2a63
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-05-10 14:51:31 -0300

    Added method send() to XHR

--- src/deferred/js/io.js
+++ src/deferred/js/io.js
@@ -3,13 +3,18 @@ if ($.ajax) {
 	var oldAjax = $.ajax;
 
 	function XHR(opts) {
-		this.promise(function (promise) {
-			opts.success = $.bind(promise.resolve, promise);
-			opts.failure = $.bind(promise.reject, promise);
-			oldAjax(opts);
-		});
+		this.config = opts || {};
 	}
 	$.extend(XHR, Deferred, {
+		
+		send: function () {
+			var opts = this.config;
+			return this.promise(function (promise) {
+				opts.success = $.bind(promise.resolve, promise);
+				opts.failure = $.bind(promise.reject, promise);
+				oldAjax(opts);
+			});
+		},
 		
 		abort: function () {
 			this.reject();
@@ -17,12 +22,13 @@ if ($.ajax) {
 		
 	});
 	
+	$.XHR = XHR;
+	
 	$.ajax = function (opts) {
 		opts = opts || {};
 		var success = opts.success;
 		var failure = opts.failure;
 		var xhr = new XHR(opts);
-		xhr.then(success, failure);
-		return xhr;
+		return xhr.send().then(success, failure);
 	};
 }
