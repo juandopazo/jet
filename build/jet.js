899a69e460492c5197727274d3fb7a49e61de329
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-05-16 11:32:28 -0300

    Updating unit tests

--- build/jet.js
+++ build/jet.js
@@ -430,7 +430,58 @@ ArrayHelper = {
 	}
 };
 
-ArrayHelper.each = ArrayHelper.forEach;var ArrayMethods,
+ArrayHelper.each = ArrayHelper.forEach;
+/**
+ * Utilities for working with object literals
+ * Throughout jet the Hash type means an object lieteral
+ * @class Hash
+ * @static
+ */
+Hash = {
+	/**
+	 * Iterats through a hash
+	 * @method each
+	 * @param {Hash} hash
+	 * @param {Function} fn
+	 * @param {Object} [thisp] Sets the value of the this keyword 
+	 */
+	each: function (hash, fn, thisp) {
+		hash = hash || {};
+		for (var x in hash) {
+			if (hash.hasOwnProperty(x)) {
+				if (fn.call(thisp || hash, x, hash[x], hash) === false) {
+					break;
+				}
+			}
+		}
+	},
+	/**
+	 * Returns an array with all the keys of a hash
+	 * @method keys
+	 * @param {Hash} hash
+	 * @return {Array}
+	 */
+	keys: function (hash) {
+		var keys = [];
+		Hash.each(hash, function (key) {
+			keys[keys.length] = key;
+		});
+		return keys;
+	},
+	/**
+	 * Returns an array with all the valus of a hash
+	 * @method values
+	 * @param {Object} hash
+	 * @return {Array}
+	 */
+	values: function (hash) {
+		var values = [];
+		Hash.each(hash, function (key, value) {
+			values[values.length] = value;
+		});
+		return values;
+	}
+};var ArrayMethods,
 	ARRAYLIST_PROTO,
 
 /**
@@ -577,57 +628,6 @@ Hash.each(ArrayMethods, function (method, returnArrayList) {
 
 ARRAYLIST_PROTO.each = ARRAYLIST_PROTO.forEach;
 
-/**
- * Utilities for working with object literals
- * Throughout jet the Hash type means an object lieteral
- * @class Hash
- * @static
- */
-Hash = {
-	/**
-	 * Iterats through a hash
-	 * @method each
-	 * @param {Hash} hash
-	 * @param {Function} fn
-	 * @param {Object} [thisp] Sets the value of the this keyword 
-	 */
-	each: function (hash, fn, thisp) {
-		hash = hash || {};
-		for (var x in hash) {
-			if (hash.hasOwnProperty(x)) {
-				if (fn.call(thisp || hash, x, hash[x], hash) === false) {
-					break;
-				}
-			}
-		}
-	},
-	/**
-	 * Returns an array with all the keys of a hash
-	 * @method keys
-	 * @param {Hash} hash
-	 * @return {Array}
-	 */
-	keys: function (hash) {
-		var keys = [];
-		Hash.each(hash, function (key) {
-			keys[keys.length] = key;
-		});
-		return keys;
-	},
-	/**
-	 * Returns an array with all the valus of a hash
-	 * @method values
-	 * @param {Object} hash
-	 * @return {Array}
-	 */
-	values: function (hash) {
-		var values = [];
-		Hash.each(hash, function (key, value) {
-			values[values.length] = value;
-		});
-		return values;
-	}
-};
 /**
  * Browser sniffing
  * @class UA
--- test/unit/node.js
+++ test/unit/node.js
@@ -9,7 +9,7 @@ test("Basic requirements", function() {
 	ok( RegExp, "RegExp" );
 	ok( jet, "jet" );
 	ok( jet(), "jet()");
-	ok( jet().use && jet().add, "jet().use && jet().add");
+	ok( jet().use && jet.add, "jet().use && jet.add");
 	jet().use(function ($) {
 		ok( $ != window.$, "jet doesn't pollute the global object" )
 	});
@@ -17,29 +17,27 @@ test("Basic requirements", function() {
 
 jet().use(function ($) {
 	
-	var Node = $.Node,
-		NodeList = $.NodeList;
+	var NodeList = $.NodeList;
 	
 	test("$ types", function () {
 		
-		ok( NodeList.is(new $.NodeList()) );
-		ok( NodeList.is($("div")), "$('div') is NodeList" );
-		ok( NodeList.is($([])), "$([]) is NodeList" );
-		ok( $([])._nodes.length == 0, "$([]) is empty NodeList" );
+		ok( $("div") instanceof $.NodeList, "$('div') is NodeList" );
+		ok( $([]) instanceof $.NodeList, "$([]) is NodeList" );
+		ok( $([])._items.length == 0, "$([]) is empty NodeList" );
 		
-		ok( Node.is($(document)), "$(document) is a Node" );
-		equals( $(document)._node, document, "$$(document)._node" );
+		ok( $(document) instanceof $.NodeList, "$(document) is a NodeList" );
+		equals( $(document)._items[0], document, "$(document)._items[0] is the document" );
 		
 		
 	});
 	
 	test("Selector", function () {
 		
-		equals($("div")._nodes.length, document.getElementsByTagName("div").length, '$("div")._nodes.length');
-		equals($("#qunit-header")._node, document.getElementById("qunit-header"), '$("#qunit-header").node');
-		equals($(".chain")._nodes.length, 10, '$(".chain")._nodes.length');
-		equals($("<div/>")._node.nodeName, "DIV", '$("<div/>")._node.nodeName');
-		equals($("<div><a href='#'>a</a></div>")._node.nodeName, "DIV", '$("<div/>")._node.nodeName');
+		equals($("div").size(), document.getElementsByTagName("div").length, '$("div").size()');
+		equals($("#qunit-header").getDOMNode(), document.getElementById("qunit-header"), '$("#qunit-header").getDOMNode()');
+		equals($(".chain").size(), 10, '$(".chain").size()');
+		equals($("<div/>").getDOMNode().nodeName, "DIV", '$("<div/>").getDOMNode().nodeName');
+		equals($("<div><a href='#'>a</a></div>").getDOMNode().nodeName, "DIV", '$("<div/>").getDOMNode().nodeName');
 		
 	});
 	
