9f48109e787dbd333ef74c40409715e1901d6598
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-06-07 12:06:59 -0300

    WidgetParent now uses ArrayList

--- src/widget-parentchild/js/WidgetParent.js
+++ src/widget-parentchild/js/WidgetParent.js
@@ -22,13 +22,16 @@ var SELECTED = 'selected',
 /**
  * A widget extension that makes the current widget contein child widgets
  * @class WidgetParent
+ * @uses ArrayList
  * @constructor
  * @param {Object} config Object literal specifying widget configuration properties
  */
-function WidgetParent() {
-	if (!this.get(CHILDREN)) {
-		this.set(CHILDREN, []);
-	}
+function WidgetParent(config) {
+	config = config || {};
+	this._items = config.children || [];
+	
+	this.on('render', this._renderChildren);
+	this.after('selectionChange', this._handleMultipleChildren);
 }
 $.mix(WidgetParent, {
 	
@@ -60,9 +63,17 @@ $.mix(WidgetParent, {
 		/**
 		 * @attribute children
 		 * @description An array of instances or configuration objects representing the widget's children
+		 * @type Array
 		 * @default []
 		 */
 		children: {
+			validator: Lang.isArray,
+			setter: function (val) {
+				this._items = val;
+			},
+			getter: function () {
+				return this._items;
+			}
 		},
 		
 		/**
@@ -123,29 +134,6 @@ $.mix(WidgetParent, {
 	},
 	
 	EVENTS: {
-		render: function () {
-			var self = this;
-			var multiple = this.get(MULTIPLE);
-			var selection = multiple ? [] : null;
-			this.forEach(this.add);
-			Hash.each(Widget.DOM_EVENTS, function (name) {
-				self.on(name, self._domEventChildrenProxy);
-			});
-			this.forEach(function (child) {
-				if (child.get(SELECTED)) {
-					if (multiple) {
-						selection.push(child);
-					} else {
-						selection = child;
-					}
-				}
-			});
-			this.set(SELECTION, selection);
-			if (!selection) {
-				this.set(SELECTED_INDEX, 0);
-			}
-		},
-		
 		afterSelectionChange: function (e, newVal) {
 			if (newVal && !this.get(MULTIPLE)) {
 				this.forEach(function (child) {
@@ -176,9 +164,35 @@ $.mix(WidgetParent, {
 });
 WidgetParent.prototype = {
 	
+	_renderChildren: function () {
+		var self = this,
+			multiple = this.get(MULTIPLE),
+			selection = multiple ? [] : null;
+			
+		this.forEach(this.add);
+		Hash.each(Widget.DOM_EVENTS, function (name) {
+			self.on(name, self._domEventChildrenProxy);
+		});
+		
+		this.forEach(function (child) {
+			if (child.get(SELECTED)) {
+				if (multiple) {
+					selection.push(child);
+				} else {
+					selection = child;
+				}
+			}
+		});
+		this.set(SELECTION, selection);
+		if (!selection) {
+			this.set(SELECTED_INDEX, 0);
+		}
+	},
+	
 	_onChildSelect: function (e) {
-		var selection = null;
-		var multiple = this.get(MULTIPLE);
+		var selection = null,
+			multiple = this.get(MULTIPLE);
+			
 		if (multiple) {
 			selection = [];
 			this.forEach(function (child) {
@@ -201,7 +215,7 @@ WidgetParent.prototype = {
 	
 	_unHookChild: function (child) {
 		child = child.get(INDEX);
-		var children = this.get(CHILDREN);
+		var children = this._items;
 		children.splice(child, 1);
 		A.forEach(children, function (item, i) {
 			if (i >= child) {
@@ -225,9 +239,9 @@ WidgetParent.prototype = {
 	 * @chainable
 	 */
 	add: function (child, index) {
-		var ChildType = this.get('childType');
-		var self = this;
-		var children = this.get(CHILDREN);
+		var ChildType = this.get('childType'),
+			self = this,
+			children = this._items;
 		if (!Lang.isNumber(index)) {
 			index = children.length;
 		}
@@ -253,16 +267,6 @@ WidgetParent.prototype = {
 	},
 	
 	/**
-	 * @method item
-	 * @description Returns a child based on an index
-	 * @param index [Number] Index of the child to be returned
-	 * @return WidgetChild
-	 */
-	item: function (index) {
-		return this.get(CHILDREN)[index || 0];
-	},
-	
-	/**
 	 * @method remove
 	 * @description Removes a child
 	 * @param {WidgetChild|Number} child The child widget or its index
@@ -273,7 +277,7 @@ WidgetParent.prototype = {
 			child = this.item(child);
 		}
 		if (child && this.fire('removeChild', { child: child })) {
-			var children = this.get(CHILDREN);
+			var children = this._items;
 			if (Lang.isNumber(child)) {
 				child = children[child];
 			}
@@ -282,28 +286,8 @@ WidgetParent.prototype = {
 			this.fire('afterRemoveChild', { child: child });
 		}
 		return this;
-	},
-	
-	/**
-	 * @method forEach
-	 * @description Iterates through all this widget children
-	 * @param {Function} fn Callback
-	 * @chainable
-	 */
-	forEach: function (fn, thisp) {
-		A.forEach(this.get(CHILDREN), fn, thisp || this);
-		return this;
-	},
-	
-	/**
-	 * @method size
-	 * @description Returns the ammount of children of this parent widget
-	 * @return Number
-	 */
-	 size: function () {
-		return this.get(CHILDREN).length;
-	 }
+	}
 	
 };
 
-WidgetParent.prototype.each = WidgetParent.prototype.forEach;
+$.mix(WidgetParent.prototype, $.ArrayList.prototype);
