77f9cd69987c679a6be18147680b20955d2c763b
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-06-22 13:51:05 -0300

    Updated Qwery

--- build/selector.js
+++ build/selector.js
@@ -20,30 +20,45 @@ jet.add('selector', function ($) {
 
   var c, i, j, k, l, m, o, p, r, v,
       el, node, len, found, classes, item, items, token,
+      html = doc.documentElement,
       id = /#([\w\-]+)/,
       clas = /\.[\w\-]+/g,
       idOnly = /^#([\w\-]+$)/,
       classOnly = /^\.([\w\-]+)$/,
       tagOnly = /^([\w\-]+)$/,
       tagAndOrClass = /^([\w]+)?\.([\w\-]+)$/,
-      html = doc.documentElement,
-      tokenizr = /\s(?![\s\w\-\/\?\&\=\:\.\(\)\!,@#%<>\{\}\$\*\^'"]*\])/,
+      normalizr = /\s*([\s\+\~>])\s*/g,
+      splitters = /[\s\>\+\~]/,
+      splittersMore = /(?![\s\w\-\/\?\&\=\:\.\(\)\!,@#%<>\{\}\$\*\^'"]*\])/,
+      dividers = new RegExp('(' + splitters.source + ')' + splittersMore.source, 'g'),
+      tokenizr = new RegExp(splitters.source + splittersMore.source),
       specialChars = /([.*+?\^=!:${}()|\[\]\/\\])/g,
       simple = /^([a-z0-9]+)?(?:([\.\#]+[\w\-\.#]+)?)/,
       attr = /\[([\w\-]+)(?:([\|\^\$\*\~]?\=)['"]?([ \w\-\/\?\&\=\:\.\(\)\!,@#%<>\{\}\$\*\^]+)["']?)?\]/,
-      chunker = new RegExp(simple.source + '(' + attr.source + ')?');
-
-  function array(ar) {
-    r = [];
-    for (i = 0, len = ar.length; i < len; i++) {
-      r[i] = ar[i];
+      pseudo = /:([\w\-]+)(\(['"]?(\w+)['"]?\))?/,
+      chunker = new RegExp(simple.source + '(' + attr.source + ')?' + '(' + pseudo.source + ')?'),
+      walker = {
+    ' ': function (node) {
+      return node && node !== html && node.parentNode
+    },
+    '>': function (node, contestant) {
+      return node && node.parentNode == contestant.parentNode && node.parentNode;
+    },
+    '~': function (node) {
+      return node && node.previousSibling;
+    },
+    '+': function (node, contestant, p1, p2) {
+      if (!node) {
+        return false;
+      }
+      p1 = previous(node);
+      p2 = previous(contestant);
+      return p1 && p2 && p1 == p2 && p1;
     }
-    return r;
-  }
-
-  var cache = function () {
-    this.c = {};
   };
+  function cache() {
+    this.c = {};
+  }
   cache.prototype = {
     g: function (k) {
       return this.c[k] || undefined;
@@ -59,11 +74,33 @@ jet.add('selector', function ($) {
       attrCache = new cache(),
       tokenCache = new cache();
 
+  function array(ar) {
+    r = [];
+    for (i = 0, len = ar.length; i < len; i++) {
+      r[i] = ar[i];
+    }
+    return r;
+  }
+
+  function previous(n) {
+    while (n = n.previousSibling) {
+      if (n.nodeType == 1) {
+        break;
+      }
+    }
+    return n
+  }
+
   function q(query) {
     return query.match(chunker);
   }
 
-  function interpret(whole, tag, idsAndClasses, wholeAttribute, attribute, qualifier, value) {
+  // this next method expect at most these args
+  // given => div.hello[title="world"]:foo('bar')
+
+  // div.hello[title="world"]:foo('bar'), div, .hello, [title="world"], title, =, world, :foo('bar'), foo, ('bar'), bar]
+
+  function interpret(whole, tag, idsAndClasses, wholeAttribute, attribute, qualifier, value, wholePseudo, pseudo, wholePseudoVal, pseudoVal) {
     var m, c, k;
     if (tag && this.tagName.toLowerCase() !== tag) {
       return false;
@@ -79,6 +116,9 @@ jet.add('selector', function ($) {
         }
       }
     }
+    if (pseudo && qwery.pseudos[pseudo] && !qwery.pseudos[pseudo](this, pseudoVal)) {
+      return false;
+    }
     if (wholeAttribute && !value) {
       o = this.attributes;
       for (k in o) {
@@ -91,22 +131,6 @@ jet.add('selector', function ($) {
       return false;
     }
     return this;
-  }
-
-  function loopAll(tokens) {
-    var r = [], token = tokens.pop(), intr = q(token), tag = intr[1] || '*', i, l, els,
-        root = tokens.length && (m = tokens[0].match(idOnly)) ? doc.getElementById(m[1]) : doc;
-    if (!root) {
-      return r;
-    }
-    els = root.getElementsByTagName(tag);
-    for (i = 0, l = els.length; i < l; i++) {
-      el = els[i];
-      if (item = interpret.apply(el, intr)) {
-        r.push(item);
-      }
-    }
-    return r;
   }
 
   function clean(s) {
@@ -128,34 +152,53 @@ jet.add('selector', function ($) {
     case '|=':
       return actual.match(attrCache.g('|=' + val) || attrCache.s('|=' + val, new RegExp('^' + clean(val) + '(-|$)')));
     }
-    return false;
+    return 0;
   }
 
   function _qwery(selector) {
-    var r = [], ret = [], i, l,
-        tokens = tokenCache.g(selector) || tokenCache.s(selector, selector.split(tokenizr));
-    tokens = tokens.slice(0);
+    var r = [], ret = [], i, j = 0, k, l, m, p, token, tag, els, root, intr, item, children,
+        tokens = tokenCache.g(selector) || tokenCache.s(selector, selector.split(tokenizr)),
+        dividedTokens = selector.match(dividers), dividedToken;
+    tokens = tokens.slice(0); // this makes a copy of the array so the cached original is not effected
     if (!tokens.length) {
       return r;
     }
-    r = loopAll(tokens);
+
+    token = tokens.pop();
+    root = tokens.length && (m = tokens[tokens.length - 1].match(idOnly)) ? doc.getElementById(m[1]) : doc;
+    if (!root) {
+      return r;
+    }
+    intr = q(token);
+    els = dividedTokens && /^[+~]$/.test(dividedTokens[dividedTokens.length - 1]) ? function (r) {
+        while (root = root.nextSibling) {
+          root.nodeType == 1 && (intr[1] ? intr[1] == root.tagName.toLowerCase() : 1) && r.push(root)
+        }
+        return r
+      }([]) :
+      root.getElementsByTagName(intr[1] || '*');
+    for (i = 0, l = els.length; i < l; i++) {
+      if (item = interpret.apply(els[i], intr)) {
+        r[j++] = item;
+      }
+    }
     if (!tokens.length) {
       return r;
     }
+
     // loop through all descendent tokens
     for (j = 0, l = r.length, k = 0; j < l; j++) {
-      node = r[j];
-      p = node;
-      // loop through each token
+      p = r[j];
+      // loop through each token backwards crawling up tree
       for (i = tokens.length; i--;) {
-        z: // loop through parent nodes
-        while (p !== html && (p = p.parentNode)) {
+        // loop through parent nodes
+        while (p = walker[dividedTokens[i]](p, r[j])) {
           if (found = interpret.apply(p, q(tokens[i]))) {
-            break z;
+            break;
           }
         }
       }
-      found && (ret[k++] = node);
+      found && (ret[k++] = r[j]);
     }
     return ret;
   }
@@ -231,6 +274,7 @@ jet.add('selector', function ($) {
       return array((root).querySelectorAll(selector));
     } :
     function (selector, root) {
+      selector = selector.replace(normalizr, '$1');
       var result = [], collection, collections = [], i;
       if (m = selector.match(tagAndOrClass)) {
         items = root.getElementsByTagName(m[1] || '*');
@@ -258,6 +302,8 @@ jet.add('selector', function ($) {
     };
 
   qwery.uniq = uniq;
+  qwery.pseudos = {};
+
   var oldQwery = context.qwery;
   qwery.noConflict = function () {
     context.qwery = oldQwery;
