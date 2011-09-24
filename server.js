2d02950f95156e120d1d1935dd2c8a57f63672bc
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-09-24 02:47:28 +0000

    Use an Express server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var comboServer = require('combohandler/lib/server'),
-    app;
+var combo = require('combohandler'),
+    express = require('express'),
+    
+    app = express.createServer();
+    
+app.get('/jet', combo.combine({rootPath: 'build/'}), function (req, res) {
+  res.send(res.body, 200);
+});
 
-app = comboServer({
-  roots: {
-    '/jet': './build'
-  }
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 app.listen(process.env.PORT || 3000);
