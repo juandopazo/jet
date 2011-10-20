0fac8b888bd0bae8d2599a0e30bcdce46aec5832
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-10-20 14:03:15 +0000

    Use typeof instead of Lang.type whenever possible

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var combo = require('combohandler'),
     
     app = express.createServer();
     
-app.get('/jet', combo.combine({rootPath: 'build/'}), function (req, res) {
-  res.send(res.body, 200);
-});
-
+//app.get('/jet', combo.combine({rootPath: 'build/'}), function (req, res) {
+//  res.send(res.body, 200);
+//});
+//
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!', 200);
 });
 
 var port = process.env.PORT || 3000;
