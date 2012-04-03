7037d6cb82552049651036d0c5a67b1f9d8e4dc4
unknown <jdopazo@.admotion.local>
2012-04-03 10:49:41 -0300

    api docs and logo

---
+++ docs/api/api.js
@@ -0,0 +1,176 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$",
+        "Button",
+        "ButtonGroup",
+        "CheckBox",
+        "Cookie",
+        "DataSource",
+        "DataSource.Ajax",
+        "DataSource.Get",
+        "DataSource.Local",
+        "DataSource.XDR",
+        "Escape",
+        "FieldSet",
+        "Form",
+        "FormField",
+        "IO",
+        "JSON",
+        "Layout",
+        "LayoutPanel",
+        "LayoutPanelBase",
+        "Log",
+        "Paginator",
+        "ProgressBar",
+        "Promise",
+        "RadioButton",
+        "RadioGroup",
+        "Record",
+        "RecordSet",
+        "SWF",
+        "SelectField",
+        "ToggleButton",
+        "TreeNode",
+        "TreeView",
+        "WidgetChild",
+        "WidgetParent"
+    ],
+    "modules": [
+        "array-extras",
+        "button",
+        "cookie",
+        "datasource",
+        "deferred",
+        "escape",
+        "form",
+        "io",
+        "io-xdr",
+        "json",
+        "layout",
+        "log",
+        "menu",
+        "node-deferred",
+        "paginator",
+        "progressbar",
+        "selector",
+        "swf",
+        "touch",
+        "treeview",
+        "widget-form",
+        "widget-parentchild"
+    ],
+    "allModules": [
+        {
+            "displayName": "array-extras",
+            "name": "array-extras",
+            "description": "Array extras based on ES5"
+        },
+        {
+            "displayName": "button",
+            "name": "button",
+            "description": "Different kinds of buttons and form elements"
+        },
+        {
+            "displayName": "cookie",
+            "name": "cookie",
+            "description": "Provides abstracion for easier use of cookies"
+        },
+        {
+            "displayName": "datasource",
+            "name": "datasource",
+            "description": "DataSources are different ways of accessing data and parsing it into an schema"
+        },
+        {
+            "displayName": "deferred",
+            "name": "deferred",
+            "description": "Provides an extension that makes a class deferrable"
+        },
+        {
+            "displayName": "escape",
+            "name": "escape",
+            "description": "Provides utility methods for escaping strings."
+        },
+        {
+            "displayName": "form",
+            "name": "form",
+            "description": "A Form widget"
+        },
+        {
+            "displayName": "io",
+            "name": "io",
+            "description": "Handles AJAX, JSONP and XSLT requests"
+        },
+        {
+            "displayName": "io-xdr",
+            "name": "io-xdr",
+            "description": "Adds functionality to make cross-domain ajax calls"
+        },
+        {
+            "displayName": "json",
+            "name": "json",
+            "description": "Adds JSON support when needed. If JSON is found natively, this module is not loaded but the $.JSON object is still found if needed"
+        },
+        {
+            "displayName": "layout",
+            "name": "layout",
+            "description": "Creates a resizable layout"
+        },
+        {
+            "displayName": "log",
+            "name": "log",
+            "description": "Base structure for logging"
+        },
+        {
+            "displayName": "menu",
+            "name": "menu",
+            "description": "OS-like menus for navigation"
+        },
+        {
+            "displayName": "node-deferred",
+            "name": "node-deferred",
+            "description": "Adds promises to Node"
+        },
+        {
+            "displayName": "paginator",
+            "name": "paginator",
+            "description": "Paginator widget"
+        },
+        {
+            "displayName": "progressbar",
+            "name": "progressbar",
+            "description": "A progressbar for informing the user of the status of loading resources. Other uses include equalizers, ratings, etc"
+        },
+        {
+            "displayName": "selector",
+            "name": "selector",
+            "description": "Small selector engine powered by Qwery"
+        },
+        {
+            "displayName": "swf",
+            "name": "swf",
+            "description": "SWFObject adapted to Jet"
+        },
+        {
+            "displayName": "touch",
+            "name": "touch",
+            "description": "Touch events"
+        },
+        {
+            "displayName": "treeview",
+            "name": "treeview",
+            "description": "TreeView module"
+        },
+        {
+            "displayName": "widget-form",
+            "name": "widget-form",
+            "description": "Provides a WidgetForm extension"
+        },
+        {
+            "displayName": "widget-parentchild",
+            "name": "widget-parentchild",
+            "description": "A module declaring extensions for parent/child relationships between nodes"
+        }
+    ]
+} };
+});
